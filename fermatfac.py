fc682e713fef525d3b8b9daaf03f5e97a66b6821
yueji1984 <48346939+yueji1984@users.noreply.github.com>
2020-10-15 15:53:31 +0800

    Update fermatfac.py

--- fermatfac.py
+++ fermatfac.py
@@ -1,7 +1,7 @@
 # Fermat’s Factorization Method, 
 # Named after Pierre de Fermat 
 # A factorization method for odd numbers n based on the representation of an odd integer as the difference of two squares	 
-# n = a^2 - b^2 = (a + b) (a - b)	and if neither of the two factors (a + b) and (a – b) equals to 1, then this is a proper factorization of n.
+# n = a^2 - b^2 = (a + b)(a - b) and if neither of the two factors (a + b) and (a – b) equals to 1, then this is a proper factorization of n.
 
 # The Fermat factorization of odd numbers follows the following steps:
 # 1)	Find the smallest integer t >= sqrt(n) 
